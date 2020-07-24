c1a4558a83826f3feb69ab3ea7ad5e6a06a83c16
Shuo <shuo@>
2020-07-24 15:56:20 +1000

    added some test scenarios

--- src/test/groovy/io/shuo/zhao/dius/shopping/CheckoutTest.groovy
+++ src/test/groovy/io/shuo/zhao/dius/shopping/CheckoutTest.groovy
@@ -4,7 +4,6 @@ import spock.lang.Specification
 import spock.lang.Subject
 
 import static io.shuo.zhao.dius.shopping.Item.*
-import static io.shuo.zhao.dius.shopping.Item.IPD
 
 class CheckoutTest extends Specification {
 
@@ -86,5 +85,42 @@ class CheckoutTest extends Specification {
         [ATV, ATV, ATV, VGA]                | BigDecimal.valueOf(249.0)
         [ATV, IPD, IPD, ATV, IPD, IPD, IPD] | BigDecimal.valueOf(2718.95)
         [MBP, VGA, IPD]                     | BigDecimal.valueOf(1949.98)
+    }
+
+    def "calculate total price - 3 for 2 apple tv deals"() {
+        expect:
+        items.each { item -> checkout.scan(item) }
+        checkout.total() == total
+
+        where:
+        items                          | total
+        [ATV, ATV, ATV]                | BigDecimal.valueOf(ATV.unitPrice).multiply(2)
+        [ATV, ATV, ATV, IPD]           | BigDecimal.valueOf(ATV.unitPrice).multiply(2).plus(IPD.unitPrice)
+        [ATV, ATV, ATV, ATV, ATV, ATV] | BigDecimal.valueOf(ATV.unitPrice).multiply(4)
+    }
+
+    def "calculate total price - IPad bulk deal"() {
+        expect:
+        items.each { item -> checkout.scan(item) }
+        checkout.total() == total
+
+        where:
+        items                          | total
+        [IPAD, IPAD, IPAD]             | BigDecimal.valueOf(IPAD.unitPrice).multiply(3)
+        [IPAD, IPAD, IPAD, IPAD]       | BigDecimal.valueOf(499.99).multiply(4)
+        [IPAD, IPAD, IPAD, IPAD, IPAD] | BigDecimal.valueOf(499.99).multiply(5)
+    }
+
+    def "calculate total price - free adapter deal"() {
+        expect:
+        items.each { item -> checkout.scan(item) }
+        checkout.total() == total
+
+        where:
+        items                | total
+        [MBP]                | BigDecimal.valueOf(IPD.unitPrice).multiply(3)
+        [MBP, VGA]           | BigDecimal.valueOf(MBP.unitPrice)
+        [MBP, VGA, MBP, VGA] | BigDecimal.valueOf(MBP.unitPrice).multiply(2)
+        [MBP, VGA, IPD]      | BigDecimal.valueOf(MBP.unitPrice).plus(IPD.unitPrice)
     }
 }
